536ffb130e178864e8b31ae94e9931607ce69b9e
Saul Padilla <saulpadilla.e@gmail.com>
2024-11-12 13:26:41 -0600

    Agregado pagina principal

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get( "/", (req, res) => {
-    res.sendFile(path.join(__dirname + "/index.html"));
+    res.sendFile(path.join(__dirname, 'public', '/html/index.html'));
 });
 
 app.listen(3000, ()=>{
